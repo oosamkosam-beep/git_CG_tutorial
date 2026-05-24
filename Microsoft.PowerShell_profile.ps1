337ad7c61d1308c4f3c90f23785f90b6b32f9c94
Osam <oosamokosam@i.softbank.jp>
2026-05-24 11:12:05 +0900

    powershellの設定ファイルの追加

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+# 出力エンコーディングをUTF-8に設定
+$OutputEncoding = [System.Text.Encoding]::UTF8
+# コンソールの入出力エンコーディングをUTF-8（65001）に設定
+[Console]::InputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -ViModeIndicator Cursor
+
+function prompt {
+    $path = Get-Location
+    Write-Host "PS" "$path" -NoNewline -ForegroundColor Cyan 
+    return "> "
+}
